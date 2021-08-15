283119bd14af6ae986f5c7632ed0812c98098b31
ksebmty <aemannuthy@gmail.com>
2021-08-16 02:21:45 +0530

    todaylast

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,19 @@
 import React from "react"
 import app from "../../base"
+import './home.css';
+import { Container, Navbar } from 'react-bootstrap'
+import logoImg from '../../asset/logo.png'
+
 
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
-            <button onClick={() => app.auth().signOut() }>Sign Out</button>
+            <Navbar className="homeNav">
+                <Container className="homeCon">
+                    <img src={logoImg} width="20px" className="homeImg" alt="/"/>
+                    <button onClick={() => app.auth().signOut() } className="signoutbtn">Sign Out</button>
+                </Container>
+            </Navbar>
         </>
     );
 };
